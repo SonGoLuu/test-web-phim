046bae5700b90363fa4a602888af4452701b0a49
Trần Huỳnh Lưu <66901066+SonGoLuu@users.noreply.github.com>
2023-07-08 22:31:02 +0700

    Add project files.

---
+++ test web phim/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace test_web_phim.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
